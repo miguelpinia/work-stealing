43254a28873de42b9abdf961d3a7f4a7cb89a360
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2022-03-12 12:03:15 -0600

    Add class definition.

--- include/ws/lib.hpp
+++ include/ws/lib.hpp
@@ -2,10 +2,35 @@
 
 #include <list>
 #include <vector>
+#include <atomic>
 
 int suma(int a, int b);
 
+
+///////////////
+// Constants //
+///////////////
+
+
+static const int EMPTY = -1;
+static const int BOTTOM = -2;
+static const int TOP = -3;
+
+
+///////////
+// Enums //
+///////////
+
+
 enum class GraphType { TORUS_2D, TORUS_2D_60, TORUS_3D, TORUS_3D_40, RANDOM, KGRAPH };
+
+enum class AlgorithmType { SIMPLE, CHASELEV, CILK, IDEMPOTENT_DEQUE, IDEMPOTENT_LIFO, IDEMPOTENT_FIFO, WS_NC_MULT_OPT, WS_NC_MULT_LA_OPT, B_WS_NC_MULT_OPT, B_WS_NC_MULT_LA_OPT };
+
+
+////////////////////////////
+// Graphs and graph utils //
+////////////////////////////
+
 
 class vertex
 {
@@ -56,7 +81,7 @@ private:
 public:
     graph();
     graph(std::vector<edge> edges, bool directed, int root, int numVertices, GraphType type);
-    // graph(bool directed, int root, int numVertices, GraphType type);
+    graph(bool directed, int root, int numVertices, GraphType type);
     ~graph();
     void addEdge(int src, int dest);
     void addEdge(edge edge);
@@ -73,6 +98,11 @@ public:
     bool isDirected();
 };
 
+
+//////////////////////////////
+// Work-stealing algorithms //
+//////////////////////////////
+
 class taskArrayWithSize
 {
 private:
@@ -84,4 +114,37 @@ public:
     int& getSize();
     int& get(int position);
     void set(int position, int value);
+};
+
+class workStealingAlgorithm
+{
+public:
+    virtual bool isEmpty();
+    virtual bool isEmpty(int label);
+    virtual void put(int task);
+    virtual bool put(int task, int label);
+    virtual int take();
+    virtual int take(int label);
+    virtual int steal();
+    virtual int stealin(int label);
+};
+
+class wsncmult : public workStealingAlgorithm
+{
+private:
+    int tail;
+    int size;
+    std::atomic<int> Head;
+    std::vector<int> head;
+    std::vector<int> tasks;
+public:
+    wsncmult(int size, int numThreads);
+    bool put(int task, int label);
+    int take(int label);
+    int steal(int label);
+    void expand();
+    void put(int task);
+    int take();
+    int steal();
+    bool isEmpty();
 };
